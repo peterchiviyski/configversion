f30fcecf60e810a2667e1684e9cd47a6b1c5cea7
peterchiviyski <peter.chiviyski@hashicorp.com>
2023-03-15 16:59:39 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+
+
+  cloud {
+    organization = "peter-working-org"
+
+    workspaces {
+      name = "testdown"
+    }
+  }
+  
+  required_providers {
+    datadog = {
+      source  = "datadog/datadog"
+      version = "~> 3.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.50.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
