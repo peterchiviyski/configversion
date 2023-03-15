37746be872bfd0be1e602dc04dac0aa523605e1e
peterchiviyski <peter.chiviyski@hashicorp.com>
2023-03-15 17:06:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.16.1"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.50.0"
-    }
   }
   required_version = ">= 1.1.0"
 }
